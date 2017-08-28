3fb973f9b0b1f899f99d471da3433ef8386d3cb8
Kitson Kelly <me@kitsonkelly.com>
2017-08-28 12:46:23 +0100

    Fix bin install

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 var tsconfig = require('./tsconfig.json');
 
 require('ts-node')
-	.register(tsconfig);
+	.register({
+		'compilerOptions': {
+			'module': 'commonjs',
+			'noUnusedLocals': true,
+			'strict': true,
+			'target': 'es2016',
+			'typeRoots': [
+				'node_modules/@types'
+			]
+		}
+	});
 
 module.exports = require('./lib/index');
